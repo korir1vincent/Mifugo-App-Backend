165da3febfcc2b1ca91611d76ba603530d7f1594
korir1vincent <korirv09@gmail.com>
2026-08-02 22:17:11 +0300

    las

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,6 +17,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  logger: true,
+  debug: true,
 });
 
 // @desc    Register new user
