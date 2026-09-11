ad8b2fc554e596cd82e9c885a3a283834fa4938e
korir1vincent <korirv09@gmail.com>
2026-09-11 12:55:45 +0300

    Added max_tokens

--- src/controllers/scanController.js
+++ src/controllers/scanController.js
@@ -141,7 +141,7 @@ If the image does not show livestock, set healthStatus to "Unable to Analyze" an
         }
       ],
       temperature: 0.1,
-      max_tokens: 800,
+      max_tokens: 2048,
       reasoning_format: "hidden"
     });
     console.log("File:", file.originalname);
